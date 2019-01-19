756db440fc0164377194cc6d4ae1e6075d183155
gikkman <ksr.jonasson@gmail.com>
2019-01-19 18:56:08 +0100

    Final fixes

--- src/main/java/com/gikk/App.java
+++ src/main/java/com/gikk/App.java
@@ -9,37 +9,44 @@ import com.gikk.chat.auto.DiceGameCommand;
 import com.gikk.chat.auto.PraiseBroadcasterCommand;
 import com.gikk.chat.auto.QuoteCommand;
 import java.util.Scanner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to this Bot example. In this example you will be able \n"
+        SpringApplication.run(App.class, args);
+    }
+    
+    @Bean
+    public CommandLineRunner run(ApplicationContext ctx) {
+        return args -> {
+            ChatService chatService = ctx.getBean(ChatService.class);
+            ChatCommandFactoryService chatCommandFactoryService = ctx.getBean(ChatCommandFactoryService.class);
+            chatService.addChatCommand(chatCommandFactoryService.create(AddBidWarCommand.class));
+            chatService.addChatCommand(chatCommandFactoryService.create(AmSubCommand.class));
+            chatService.addChatCommand(chatCommandFactoryService.create(BiggestLoserCommand.class));
+            chatService.addChatCommand(chatCommandFactoryService.create(CommandsCommand.class));
+            chatService.addChatCommand(chatCommandFactoryService.create(DiceGameCommand.class));
+            chatService.addChatCommand(chatCommandFactoryService.create(PraiseBroadcasterCommand.class));
+            chatService.addChatCommand(chatCommandFactoryService.create(QuoteCommand.class));
+
+            //As long as we don't type .quit into the command prompt, send everything we type as a message to twitch
+           System.out.println("Welcome to this Bot example. In this example you will be able \n"
                 + "to send and receive messages from a Twitch chat channel. You will \n"
                 + "make all input directly here in the command prompt. \n"
                 + "I hope you set the credentials in the SystemConfig.java file ^.^\n\n"
                 + "Type .quite to end the example. \n\n");
-        
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ChatService chatService = ctx.getBean(ChatService.class);
-        ChatCommandFactoryService chatCommandFactoryService = ctx.getBean(ChatCommandFactoryService.class);
-        chatService.addChatCommand(chatCommandFactoryService.create(AddBidWarCommand.class));
-        chatService.addChatCommand(chatCommandFactoryService.create(AmSubCommand.class));
-        chatService.addChatCommand(chatCommandFactoryService.create(BiggestLoserCommand.class));
-        chatService.addChatCommand(chatCommandFactoryService.create(CommandsCommand.class));
-        chatService.addChatCommand(chatCommandFactoryService.create(DiceGameCommand.class));
-        chatService.addChatCommand(chatCommandFactoryService.create(PraiseBroadcasterCommand.class));
-        chatService.addChatCommand(chatCommandFactoryService.create(QuoteCommand.class));
-        
-        //As long as we don't type .quit into the command prompt, send everything we type as a message to twitch
-        try (Scanner scanner = new Scanner(System.in)) {
-            String line;
-            while (!(line = scanner.nextLine()).matches(".quit")) {
-                chatService.broadcast(line);
+            try (Scanner scanner = new Scanner(System.in)) {
+                String line;
+                while (!(line = scanner.nextLine()).matches(".quit")) {
+                    chatService.broadcast(line);
+                }   
             }
-        }
-        ctx.close();
+        };
     }
 }
--- src/main/java/com/gikk/SystemConfig.java
+++ src/main/java/com/gikk/SystemConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "bot")
 public class SystemConfig {
-
     @NotBlank
     public String password;
     @NotBlank
@@ -16,20 +15,36 @@ public class SystemConfig {
     public String username;
     @NotBlank
     public String currency;
-
+    
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getChannel() {
         return channel;
     }
 
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getCurrency() {
         return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
     }
 }
